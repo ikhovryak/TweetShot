deead7b72d7e19d5f99a81a599d2ef9d0ee33f81
Oleh Shostak <43040476+oshostak@users.noreply.github.com>
2020-11-22 00:03:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,27 @@
 /* global createCanvas colorMode HSB frameRate background width height stroke
    noFill noStroke keyCode rect UP_ARROW DOWN_ARROW RIGHT_ARROW LEFT_ARROW */
 
-let windowX, windowY, selectionX, selectionY, locked, shotLocked, startedX, startedY, finalX, finalY, deltaY, imageBase64, working;
+let windowX, windowY, selectionX, selectionY, locked, shotLocked, startedX, startedY, finalX, finalY, deltaY, imageBase64, working, empty;
 
-// windowX = sketch.window.innerWidth;
-// windowY = sketch.window.innerHeight;
-
-// function activate(){
-//     working = true;
-//     console.log('BLIAT')
-// }
-
-// 1. Create the button
-
-// document.addEventListener('DOMContentLoaded', function(){
-//     var checkPageButton = document.getElementById('activateButton');
-//     checkPageButton.addEventListener('click', function(){
-//         chrome.tabs.getSelected(null, function(tab){
-//             working = true;
-//             alert(working);
-//         })
-//     })
-// })
-// myActivateButton = document.querySelector("button");
-// myActivateButton.onclick = activate;
-
-// addEventListener("click", activate);
+chrome.storage.local.get('working', (response) => {
+    if (response.working) {
+        empty = 0;
+    }
+  });
+  chrome.runtime.onMessage.addListener(request => {
+    if (request.working) {
+      working = true;
+    }
+  });
 
 var s = function(sketch){
 
     sketch.setup = function() {
-        // Canvas & color settings
-        var cnv = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
-        cnv.position(0, 0, 'fixed');
+        if (working){
+            // Canvas & color settings
+            var cnv = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+            cnv.position(0, 0, 'fixed');
+        }
         deltaY = 0;
     }
 
@@ -41,9 +30,7 @@ var s = function(sketch){
     }
 
     sketch.mousePressed = function() {
-        alert(working);
         shotLocked = false;
-
         if (sketch.mouseX < sketch.windowWidth && sketch.mouseY < sketch.windowHeight && sketch.mouseX >= 0 && sketch.mouseY >= 0 && working == true) {
             locked = true;
             startedX = sketch.mouseX;
@@ -73,14 +60,6 @@ var s = function(sketch){
             working = false;
         }
     }
-    // class Snake {
-    //     constructor() {
-    //     this.size = 10;
-    //     }
-
-    //     moveSelf() {
-    //     }
-    // }
 
     function logFinal(){
         console.log('startedX', startedX)
